2dc9f78f6c11add2d6b5dd91c1347a69acc73dfa
azemitis <7-days@inbox.lv>
2023-07-07 10:57:13 +0300

    Balance auto update functionality

--- resources/views/deposits/index.blade.php
+++ resources/views/deposits/index.blade.php
@@ -18,13 +18,13 @@
                                 <table class="w-full bg-white rounded-lg overflow-hidden shadow-lg">
                                     <thead class="bg-indigo-200">
                                     <tr>
-                                        <th class="px-4 py-2 text-left text-gray-800 font-semibold border">Account
-                                            Number
+                                        <th class="px-4 py-2 text-left text-gray-800 font-semibold border">
+                                            Account Number
                                         </th>
                                         <th class="px-4 py-2 text-left text-gray-800 font-semibold border">Currency</th>
                                         <th class="px-4 py-2 text-left text-gray-800 font-semibold border">Term</th>
-                                        <th class="pl-4 py-2 text-left text-gray-800 font-semibold border">Amount
-                                            Deposited
+                                        <th class="pl-4 py-2 text-left text-gray-800 font-semibold border">
+                                            Amount Deposited
                                         </th>
                                         <th class="px-4 py-2 text-left text-gray-800 font-semibold border">Amount Due
                                         </th>
@@ -47,7 +47,8 @@
                                                     @method('DELETE')
                                                     <button type="submit" class="bg-gray-400 hover:bg-red-600
                                                     text-white font-bold py-2 px-4 rounded focus:outline-none
-                                                    focus:shadow-outline">Withdraw</button>
+                                                    focus:shadow-outline">Withdraw
+                                                    </button>
                                                 </form>
                                             </td>
                                         </tr>
@@ -74,8 +75,9 @@
                                             <th class="px-4 py-2 text-left text-gray-800 font-semibold border">From
                                                 Account:
                                             </th>
-                                            <th class="px-4 py-2 text-left text-gray-800 font-semibold border">
-                                                Available Balance:</th>
+                                            <th class="px-4 py-2 text-left text-gray-800 font-semibold border">Available
+                                                Balance:
+                                            </th>
                                             <th class="px-4 py-2 text-left text-gray-800 font-semibold border">
                                                 Currency:
                                             </th>
@@ -102,15 +104,17 @@
                                             </td>
 
                                             <td class="border border-gray-300 px-4 py-2">
-                                                <input type="text" readonly value="{{ $account->balance }}"
+                                                <input type="text" readonly id="available_balance"
+                                                       value="{{ $accounts[0]->balance }}"
                                                        class="border rounded py-2 px-3 text-gray-700 bg-gray-100
-                                                       focus:outline-none focus:shadow-outline w-28">
+                                                       focus:outline-none focus:shadow-outline w-28"
+                                                       onchange="updateAccountBalance()">
                                             </td>
 
                                             <td class="border border-gray-300 px-4 py-2">
                                                 <select name="currency" id="currency"
                                                         class="border rounded py-2 px-3 text-gray-700 focus:outline-none
-                                                        focus:shadow-outline  w-20">
+                                                        focus:shadow-outline w-20">
                                                     <option value="EUR">EUR</option>
                                                     <option value="USD">USD</option>
                                                     <option value="GBP">GBP</option>
@@ -120,7 +124,7 @@
                                             <td class="border border-gray-300 px-4 py-2">
                                                 <select name="term" id="term"
                                                         class="border rounded py-2 px-3 text-gray-700 focus:outline-none
-                                            focus:shadow-outline  w-28">
+                                                        focus:shadow-outline  w-28">
                                                     @foreach ($terms as $term)
                                                         <option value="{{ $term }}">{{ $term }} months</option>
                                                     @endforeach
@@ -130,18 +134,17 @@
 
                                             <td class="border border-gray-300 px-4 py-2">
                                                 <input type="text" name="amount" id="amount"
-                                                       class="border rounded py-2 px-3 text-gray-700
-                                                       focus:outline-none focus:shadow-outline w-24">
+                                                       class="border rounded py-2 px-3 text-gray-700 focus:outline-none
+                                                       focus:shadow-outline w-24">
                                             </td>
 
                                             <td class="border border-gray-300 px-4 py-2">
                                                 <button type="submit"
-                                                        class="bg-indigo-500 text-sm hover:bg-indigo-600 text-white font-bold
-                                                        py-2  rounded focus:outline-none focus:shadow-outline w-32">
+                                                        class="bg-indigo-500 text-sm hover:bg-indigo-600 text-white
+                                                        font-bold py-2 rounded focus:outline-none focus:shadow-outline w-32">
                                                     Open Deposit Account
                                                 </button>
                                             </td>
-
                                         </tr>
                                         </tbody>
                                     </table>
@@ -182,6 +185,20 @@
             }, 2000);
         }
 
+        <!-- Update Account Balance -->
+        const accountDropdown = document.getElementById('from_account');
+        const availableBalanceField = document.getElementById('available_balance');
+
+        function updateAccountBalance() {
+            const selectedAccountId = accountDropdown.value;
+            const selectedAccount = {!! json_encode($accounts) !!}
+                .find(account => account.id === parseInt(selectedAccountId));
+            availableBalanceField.value = selectedAccount.balance;
+        }
+
+        <!-- Event listener for account dropdown change -->
+        accountDropdown.addEventListener('change', updateAccountBalance);
+
         <!-- Show success message -->
         const successMessage = document.getElementById('flash-success-message');
         if (successMessage) {
@@ -194,6 +211,8 @@
             showFlashMessage('flash-error-message', 'flash-error');
         }
 
+        <!-- Update account balance on page load -->
+        window.addEventListener('DOMContentLoaded', updateAccountBalance);
     </script>
 
 </x-app-layout>
