06be6214f38656a87171a06f88d58021b3fd674c
azemitis <7-days@inbox.lv>
2023-07-08 18:07:16 +0300

    Crypto list

--- app/Http/Controllers/CryptoController.php
+++ app/Http/Controllers/CryptoController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class CryptoController extends Controller
 {
     public function index()
     {
-        return view('crypto.index');
+        $apiKey = env('CMC_API_KEY');
+        $url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
+
+        try {
+            $response = Http::withHeaders([
+                'X-CMC_PRO_API_KEY' => $apiKey,
+            ])->get($url);
+
+            $data = $response->json();
+            $cryptocurrencies = $data['data'];
+
+            foreach ($cryptocurrencies as &$cryptocurrency) {
+                $logoUrl = $this->getLogoUrl($cryptocurrency['symbol']);
+                $cryptocurrency['logo_url'] = $logoUrl;
+            }
+
+            return view('crypto.index', compact('cryptocurrencies'));
+        } catch (\Exception $e) {
+            return back()->withErrors('Failed to fetch cryptocurrency data.');
+        }
+    }
+
+    private function getLogoUrl($symbol)
+    {
+        $baseUrl = 'https://s2.coinmarketcap.com/static/img/coins/64x64';
+        return $baseUrl . '/' . $symbol . '.png';
     }
 }
--- resources/views/crypto/index.blade.php
+++ resources/views/crypto/index.blade.php
@@ -9,8 +9,43 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-indigo-100 border-b border-gray-200">
-                    <h4 class="text-xl font-semibold text-gray-800">Welcome to the Cryptocurrency Page</h4>
-                    <p>Information, charts, and other content related to cryptocurrencies.</p>
+                    <h4 class="text-xl font-semibold text-gray-800">Discover Cryptocurrencies</h4>
+                    <p>Explore the world of cryptocurrencies, including market data, price charts, and trends.
+                        Buy or sell your cryptocurrencies.</p>
+
+                    <!-- Display table -->
+                    <table class="w-full bg-white rounded-lg overflow-hidden shadow-lg mt-6">
+                        <thead class="bg-indigo-200">
+                        <tr>
+                            <th class="px-4 py-2 text-left text-gray-800 font-semibold border">Name</th>
+                            <th class="px-4 py-2 text-left text-gray-800 font-semibold border">Symbol</th>
+                            <th class="px-4 py-2 text-left text-gray-800 font-semibold border">Price (USD)</th>
+                            <th class="px-4 py-2 text-left text-gray-800 font-semibold border">Changes last 1h</th>
+                            <th class="px-4 py-2 text-left text-gray-800 font-semibold border">Purchase</th>
+                            <th class="px-4 py-2 text-left text-gray-800 font-semibold border">Sell</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($cryptocurrencies as $cryptocurrency)
+                            <tr class="border-b border-gray-300">
+                                <td class="px-4 py-2 border">{{ $cryptocurrency['name'] }}</td>
+                                <td class="px-4 py-2 border">{{ $cryptocurrency['symbol'] }}</td>
+                                <td class="px-4 py-2 border">{{ $cryptocurrency['quote']['USD']['price'] }}</td>
+                                <td class="px-4 py-2
+                                        @if ($cryptocurrency['quote']['USD']['percent_change_1h'] < 0)
+                                            text-red-500
+                                        @else
+                                            text-green-500
+                                        @endif
+                                    ">
+                                    {{ $cryptocurrency['quote']['USD']['percent_change_1h'] }}
+                                </td>
+                                <td class="px-4 py-2 border">Purchase</td>
+                                <td class="px-4 py-2 border">Sell</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
